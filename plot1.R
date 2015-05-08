420a6ca948942181df7c015ad29fd20bfb3e095c
lintu <lintus@users.noreply.github.com>
2015-05-08 17:48:46 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,10 @@
-1 dataFile <- "./data/household_power_consumption.txt" 
-2 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".") 
-3 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,] 
-4 
+dataFile <- "./data/household_power_consumption.txt" 
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".") 
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,] 
  
-5 #str(subSetData) 
-6 globalActivePower <- as.numeric(subSetData$Global_active_power) 
-7 png("plot1.png", width=480, height=480) 
-8 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)") 
-9 dev.off() 
+ 
+#str(subSetData) 
+globalActivePower <- as.numeric(subSetData$Global_active_power) 
+png("plot1.png", width=480, height=480) 
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)") 
+dev.off() 
